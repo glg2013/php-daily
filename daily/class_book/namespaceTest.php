ff11fdcb0360336c25cfa8aa421cb7f914192ec4
fairy_shrimp <fengniancong@163.com>
2023-04-21 10:28:48 +0800

    练习命名空间、导入别名，思考命名冲突

--- daily/class_book/namespaceTest.php
+++ daily/class_book/namespaceTest.php
@@ -1,32 +1,31 @@
 <?php
 
-/*
-namespace main;
+//namespace main;
+//
+//require_once "Debug.php";
+//use com\getinstance\util\Debug as uDebug;
+//
+//class Debug {
+//    public static function helloWorld()
+//    {
+//        print "hello from main\Debug";
+//    }
+//}
+//
+////\com\getinstance\util\Debug::helloWorld();
+//uDebug::helloWorld();
+//Debug::helloWorld();
 
-require_once "Debug.php";
-use com\getinstance\util\Debug as uDebug;
-
-class Debug {
-    public static function helloWorld()
-    {
-        print "hello from main\Debug";
-    }
-}
-
-uDebug::helloWorld();
-Debug::helloWorld();
-*/
-
-namespace com\getinstance\util;
-require_once "global.php";
-
-class Lister
-{
-    public static function helloWorld()
-    {
-        print "hello from " . __NAMESPACE__ . "\n";
-    }
-}
-
-Lister::helloWorld();
-\Lister::helloWorld();
+//namespace com\getinstance\util;
+//require_once "global.php";
+//
+//class Lister
+//{
+//    public static function helloWorld()
+//    {
+//        print "hello from " . __NAMESPACE__ . "\n";
+//    }
+//}
+//
+//Lister::helloWorld();
+//\Lister::helloWorld();
