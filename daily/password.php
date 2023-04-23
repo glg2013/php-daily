56e8ece12d05f2431dfe54d1d60eb962054b8916
fairy_shrimp <fengniancong@163.com>
2023-04-23 14:37:53 +0800

    hello.php 内容更新为输出 phpinfo password.php 运行查看函数的使用的方法

--- daily/password.php
+++ daily/password.php
@@ -2,7 +2,8 @@
 
 $password = 'goodgoodstudy!';
 $passwordHash = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
-//var_dump($passwordHash);
+//var_dump($passwordHash);print strlen($passwordHash);exit();
+//$passwordHash = '$2y$12$n3P136l8uSUHWqxGCe/3V.lxNzuJa3LbgSFia92JgD0slpcTglIP.';
 
 if (password_verify($password, $passwordHash)) {
     echo '密码一致';exit();
--- hello.php
+++ hello.php
@@ -1,11 +1,3 @@
 <?php
 
-$msg = '世界你好！abc';
-
-$info = '';
-for ($i = 0; $i < mb_strlen($msg); $i++) {
-    $m = mb_substr($msg, -($i+1), 1);
-    $info .= $m;
-}
-
-echo $info;
+echo phpinfo();
